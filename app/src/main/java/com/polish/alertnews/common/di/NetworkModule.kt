735086d0eb82a305562ccae115b728cbfd071ec2
Osehi <osehiaseehilen@gmail.com>
2022-10-20 14:06:38 +0100

    Provided okHttp object

--- app/src/main/java/com/polish/alertnews/common/di/NetworkModule.kt
+++ app/src/main/java/com/polish/alertnews/common/di/NetworkModule.kt
@@ -1,11 +1,16 @@
 package com.polish.alertnews.common.di
 
+import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
+@Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
     /**
@@ -16,5 +21,26 @@ object NetworkModule {
     @Singleton
     fun provideLogger(): HttpLoggingInterceptor {
         return HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+    }
+    /**
+     * provide gsonconverter
+     */
+    @Provides
+    @Singleton
+    fun provideGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+    /**
+     * provide the OkHttp
+     */
+    fun provideOkHttp(
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(60L, TimeUnit.SECONDS)
+            .readTimeout(60L, TimeUnit.SECONDS)
+            .writeTimeout(60L, TimeUnit.SECONDS)
+            .addInterceptor(loggingInterceptor)
+            .build()
     }
 }
