dec8aa5741dee5f9a946ddf4ed9e2c3d8c064d93
Osehi <osehiaseehilen@gmail.com>
2022-10-20 13:22:00 +0100

    Added httpLogger

---
+++ app/src/main/java/com/polish/alertnews/common/di/NetworkModule.kt
@@ -0,0 +1,20 @@
+package com.polish.alertnews.common.di
+
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    /**
+     * provide the logger
+     * this logs out every response to and from the web service
+     */
+    @Provides
+    @Singleton
+    fun provideLogger(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+    }
+}
